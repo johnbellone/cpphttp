7f95cac9b96a45461af6b5b6c9566d2b1562c504
John Bellone <jb@thunkbrightly.com>
2011-07-30 11:03:33 -0400

    Now using boost libraries for asio

--- main.cc
+++ main.cc
@@ -3,66 +3,64 @@
 #include <string>
 #include <sstream>
 
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <cstdlib>
-#include <cstring>
-#include <unistd.h>
-
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
 #include <boost/program_options.hpp>
 
 namespace po = boost::program_options;
+namespace ba = boost::asio;
 
-static void close_socket(int sd)
+class simple_http {
+    ba::io_service io_service;
+    ba::ip::tcp::endpoint endpoint;
+    ba::ip::tcp::acceptor acceptor;
+    ba::ip::tcp::socket sock;
+    std::string data;
+
+    void write_handler(const boost::system::error_code& ec, std::size_t tx);;
+    void accept_handler(const boost::system::error_code& ec);
+public:
+    simple_http(const std::string& message, int port);
+
+    void run();
+};
+
+simple_http::simple_http(const std::string& message, int port)
+    : io_service()
+    , endpoint(ba::ip::tcp::v4(), port)
+    , acceptor(io_service, endpoint)
+    , sock(io_service)
+    , data(message)
 {
-    if (sd > 0)
+    acceptor.listen();
+    acceptor.async_accept(sock, boost::bind(&simple_http::accept_handler, this, ba::placeholders::error));
+
+    std::cout << "Ready to rock and roll on port " << port << std::endl;;
+}
+
+void simple_http::run()
+{
+    io_service.run();
+}
+
+/*private*/ void simple_http::write_handler(const boost::system::error_code& ec, std::size_t tx)
+{
+    if (!ec)
     {
-        close(sd);
-        sd = -1;
+        std::cout << "Wrote "<< tx << " bytes" << std::endl;
     }
 }
 
-static int createSocket(int port)
+/*private*/ void simple_http::accept_handler(const boost::system::error_code& ec)
 {
-    const int MAX_PENDING = 5;
-    int sd = -1;
-    try
+    if (!ec)
     {
-        if ((sd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-        {
-            std::ostringstream ss;
-            ss<<"Failed creating socket on port "<<port<<std::endl;
-            throw std::runtime_error(ss.str());
-        }
-
-        struct sockaddr_in saddr;
-        std::memset(&saddr, 0, sizeof(saddr));
-        saddr.sin_family = AF_INET;
-        saddr.sin_addr.s_addr = htonl(INADDR_ANY);
-        saddr.sin_port = htons(port);
-
-        if (bind(sd, (struct sockaddr*)&saddr, sizeof(saddr)) < 0)
-        {
-            std::ostringstream ss;
-            ss<<"Failed binding socket on port "<<port<<std::endl;
-            throw std::runtime_error(ss.str());
-        }
-
-        if (listen(sd, MAX_PENDING) < 0)
-        {
-            std::ostringstream ss;
-            ss<<"Failed listening socket on port "<<port<<std::endl;
-            throw std::runtime_error(ss.str());
-        }
-
-        std::cout<<"Ready to rock and roll on port "<<port<<std::endl;
+        ba::async_write(sock, ba::buffer(data), 
+                        boost::bind(&simple_http::write_handler, 
+                                    this,
+                                    ba::placeholders::error,
+                                    ba::placeholders::bytes_transferred));
     }
-    catch (const std::exception& e)
-    {
-        throw;
-    }
-
-    return sd;
 }
 
 int main(int argc, char* argv[]) 
@@ -79,8 +77,6 @@ int main(int argc, char* argv[])
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
 
-    int sd = -1;
-
     std::ostringstream response;
     response<<"HTTP/1.0 200 OK\r\n"
             <<"Content-Type: text/html; charset=UTF-8\r\n"
@@ -88,55 +84,14 @@ int main(int argc, char* argv[])
             <<"\r\n"<<message<<"\r\n";
     message = response.str();
 
-    try
-    {
-        sd = createSocket(port);
-
-        char buf[1025]; buf[1024] = 0;
-        struct sockaddr_in client;
-        unsigned int len = sizeof(client), c_sd = -1;
-
-        while (8)
-        {
-            if ((c_sd = accept(sd, (struct sockaddr*)&client, &len)) < 0)
-            {
-                std::ostringstream ss;
-                ss<<"Failed on accept()"<<std::endl;
-                throw std::runtime_error(ss.str());
-            }
-
-            int rx = -1;
-            if ((rx = recv(c_sd, buf, 1024, 0)) < 0)
-            {
-                std::cerr<<"Failed reading from "
-                         <<inet_ntoa(client.sin_addr)
-                         <<std::endl;
-            }
-
-            std::cout<<"RX> ("<<inet_ntoa(client.sin_addr)<<") "
-                     <<std::endl<<buf<<std::endl
-                     <<std::flush;
-
-            int tx = message.length();
-            if (send(c_sd, message.c_str(), tx, 0) != tx)
-            {
-                std::cerr<<"Error on sending message to client from "
-                         <<inet_ntoa(client.sin_addr)
-                         <<std::endl;
-            }
-            else
-            {
-                std::cout<<"TX> "<<message<<std::endl;
-            }
-
-            close_socket(c_sd);
-        }
+    try {
+        simple_http service(message, port);
+        service.run();
     }
     catch (const std::exception& e)
     {
         std::cerr<<e.what()<<std::endl; // spew
     }
 
-    close_socket(sd);
     return 0;
 }
